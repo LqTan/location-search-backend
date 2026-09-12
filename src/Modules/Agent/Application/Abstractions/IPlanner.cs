abdb0c9cafd54e8e375ec13dfe87446cac7e9217
tan.le <lequantan1974@gmail.com>
2026-09-13 00:23:32 +0700

    remove old module agent

--- src/Modules/Agent/Application/Abstractions/IPlanner.cs
+++
@@ -1,8 +0,0 @@
-using Agent.Domain;
-
-namespace Agent.Application.Abstractions;
-
-public interface IPlanner
-{
-    AgentPlan CreatePlan(string query, bool hasUserLocation);
-}
--- src/Modules/Agent/Domain/AgentAction.cs
+++
@@ -1,8 +0,0 @@
-namespace Agent.Domain;
-
-public enum AgentAction
-{
-    SearchPlace = 0,
-    SavePreference = 1,
-    CreateReview = 2
-}
